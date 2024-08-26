847a4c1259471357121a2d79dcb417dfaf39dd34
ben mccarthy <bmccarthy90@gmail.com>
2024-08-26 16:29:31 -0400

    passing fibbionacci test

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    for n in range(length):
+        if n > 1:
+            fib_list.append(fib_list[-1] + fib_list[-2])
+        else:
+            fib_list.append(n)
+
+    print(fib_list)
+    
+# print_fibonacci(9)
